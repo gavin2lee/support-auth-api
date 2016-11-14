a1cb13abd89255a3f66989114fc510e2b2ea16fd
gavin2lee <gavin2lee@163.com>
2016-11-14 15:26:38 +0800

    api

--- src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
+++ src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
@@ -1,5 +1,8 @@
 package com.lachesis.support.auth.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,24 @@ import com.lachesis.support.auth.api.vo.AuthResponse;
 public class AuthCenterController {
 	private static final Logger LOG = LoggerFactory.getLogger(AuthCenterController.class);
 	
-	@RequestMapping("")
-	public AuthResponse requestToken(){
+	@RequestMapping("token")
+	public AuthResponse requestToken(String username, String password, HttpServletRequest request){
+		if(LOG.isInfoEnabled()){
+			LOG.info(String.format("request token for [username:%s]", username));
+		}
+		
+		if(isBlank(username) || isBlank(password)){
+			
+		}
 		return null;
 	}
 	
 	@RequestMapping("")
 	public AuthResponse verifyToken(String token,String ip){
 		return null;
+	}
+	
+	private boolean isBlank(String s){
+		return StringUtils.isBlank(s);
 	}
 }
