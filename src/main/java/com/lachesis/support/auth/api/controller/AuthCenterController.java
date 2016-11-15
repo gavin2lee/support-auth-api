920bfd367ad6984e6e20dd765b53410425f99139
gavin2lee <gavin2lee@163.com>
2016-11-15 13:12:52 +0800

    rest

--- src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
+++ src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lachesis.support.auth.api.common.AuthBizErrorCodes;
@@ -23,74 +24,74 @@ import com.lachesis.support.auth.vo.UserDetails;
 @RequestMapping("/")
 public class AuthCenterController {
 	private static final Logger LOG = LoggerFactory.getLogger(AuthCenterController.class);
-	
+
 	@Autowired
 	private CentralizedAuthSupporter authSupporter;
-	
+
 	@RequestMapping("token")
-	public AuthResponse requestToken(@RequestBody TokenRequest tokenRequest, HttpServletRequest request){
-		if(LOG.isInfoEnabled()){
+	public AuthResponse requestToken(@RequestBody TokenRequest tokenRequest, HttpServletRequest request) {
+		if (LOG.isInfoEnabled()) {
 			LOG.info(String.format("request token for [username:%s]", tokenRequest.getUsername()));
 		}
-		
-		if(isBlank(tokenRequest.getUsername()) || isBlank(tokenRequest.getPassword())){
+
+		if (isBlank(tokenRequest.getUsername()) || isBlank(tokenRequest.getPassword())) {
 			LOG.error(String.format("errors with [username:%s]", tokenRequest.getUsername()));
 			throw new AuthenticationException(AuthBizErrorCodes.AUTH_FAILED_ARGS, "用户名或密码为空");
 		}
-		
+
 		String userid = tokenRequest.getUsername();
 		String psword = tokenRequest.getPassword();
 		String ip = determineTerminalIpAddress(request);
-		
+
 		String token = authSupporter.generateToken(userid, psword, ip);
-		if(isBlank(token)){
+		if (isBlank(token)) {
 			LOG.error(String.format("authenticating failed for [userid:%s, ip:%s]", userid, ip));
 			throw new AuthenticationException(AuthBizErrorCodes.AUTH_FAILED, "认证错误");
 		}
-		
+
 		UserDetails userDetails = authSupporter.authenticate(token, ip);
-		if(userDetails == null){
-			LOG.error("cannot get userdetails with token:"+token);
+		if (userDetails == null) {
+			LOG.error("cannot get userdetails with token:" + token);
 			throw new AuthenticationException(AuthBizErrorCodes.AUTH_FAILED, "认证失败");
 		}
-		
+
 		String resourceId = userDetails.getResourceId();
-		
+
 		TokenResponse tokenResp = new TokenResponse();
 		tokenResp.setToken(token);
 		tokenResp.setUserId(resourceId);
 		return tokenResp;
 	}
-	
-	@RequestMapping("authentication")
-	public AuthResponse verifyToken(String token,String ip){
-		if(LOG.isInfoEnabled()){
+
+	@RequestMapping("auth")
+	public AuthResponse verifyToken(@RequestParam("token") String token, @RequestParam("ip") String ip) {
+		if (LOG.isInfoEnabled()) {
 			LOG.info(String.format("authentication for [token:%s,ip:%s]", token, ip));
 		}
-		
-		if(isBlank(token) || isBlank(ip)){
+
+		if (isBlank(token) || isBlank(ip)) {
 			LOG.error(String.format("errors with [token:%s, ip:%s]", token, ip));
 			throw new AuthenticationException(AuthBizErrorCodes.AUTH_FAILED_ARGS, "token或IP为空");
 		}
-		
+
 		UserDetails userDetails = authSupporter.authenticate(token, ip);
-		if(userDetails == null){
+		if (userDetails == null) {
 			LOG.error(String.format("authentication failed with [token:%s, ip:%s]", token, ip));
 			throw new AuthenticationException(AuthBizErrorCodes.AUTH_FAILED_TOKEN, "无效token");
 		}
-		
+
 		UserDetailsResponse resp = new UserDetailsResponse();
 		resp.setUserId(userDetails.getResourceId());
 		resp.setUsername(userDetails.getUserid());
 		return resp;
 	}
-	
-	private String determineTerminalIpAddress(HttpServletRequest request){
+
+	private String determineTerminalIpAddress(HttpServletRequest request) {
 		String ip = request.getRemoteAddr();
 		return ip;
 	}
-	
-	private boolean isBlank(String s){
+
+	private boolean isBlank(String s) {
 		return StringUtils.isBlank(s);
 	}
 }
