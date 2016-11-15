6a5e37a50324672f631839fe106a4ef6758747a1
gavin2lee <gavin2lee@163.com>
2016-11-15 09:25:15 +0800

    format

--- src/main/java/com/lachesis/support/auth/api/exception/LocalResponseStatusExceptionResolver.java
+++ src/main/java/com/lachesis/support/auth/api/exception/LocalResponseStatusExceptionResolver.java
@@ -8,30 +8,30 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
-public class LocalResponseStatusExceptionResolver extends AbstractHandlerExceptionResolver{
+public class LocalResponseStatusExceptionResolver extends AbstractHandlerExceptionResolver {
 
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception ex) {
-		
-		try{
-		if(ex instanceof AuthenticationException){
-			AuthenticationException authEx = (AuthenticationException)ex;
-			response.setContentType("application/json; charset=utf-8");
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			PrintWriter writer = response.getWriter();
-			writer.write(convertToJson(authEx));
-			writer.close();
-//			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, );
+
+		try {
+			if (ex instanceof AuthenticationException) {
+				AuthenticationException authEx = (AuthenticationException) ex;
+				response.setContentType("application/json; charset=utf-8");
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				PrintWriter writer = response.getWriter();
+				writer.write(convertToJson(authEx));
+				writer.close();
+				// response.sendError(HttpServletResponse.SC_UNAUTHORIZED, );
+			}
+		} catch (Exception handlerEx) {
+
 		}
-		}catch(Exception handlerEx){
-			
-		}
-		
+
 		return new ModelAndView();
 	}
-	
-	private String convertToJson(AuthenticationException ex){
+
+	private String convertToJson(AuthenticationException ex) {
 		String template = "{\"subcode\":\"%s\",\"submsg\":\"%s\"}";
 		String ret = String.format(template, ex.getCode(), ex.getMessage());
 		return ret;
