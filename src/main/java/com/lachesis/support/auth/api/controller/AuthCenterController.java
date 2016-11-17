97023ec1eb465e97518b993c02b091f48dc0029a
gavin2lee <gavin2lee@163.com>
2016-11-17 16:19:32 +0800

    auth

--- src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
+++ src/main/java/com/lachesis/support/auth/api/controller/AuthCenterController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -89,8 +90,8 @@ public class AuthCenterController {
 		return resp;
 	}
 	
-	@RequestMapping(value="token",produces={MediaType.APPLICATION_JSON_VALUE},method=RequestMethod.DELETE)
-	public void logout(@RequestParam("token")String token, HttpServletRequest request){
+	@RequestMapping(value="token/{tokenid}",produces={MediaType.APPLICATION_JSON_VALUE},method=RequestMethod.DELETE)
+	public void logout(@PathVariable("tokenid")String token, HttpServletRequest request){
 		authSupporter.logout(token);
 	}
 
