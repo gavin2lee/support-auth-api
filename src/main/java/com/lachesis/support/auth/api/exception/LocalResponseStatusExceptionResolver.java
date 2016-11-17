e0d49a87ef403dadbcabec110fc686517ec3179f
gavin2lee <gavin2lee@163.com>
2016-11-17 13:09:35 +0800

    api

--- src/main/java/com/lachesis/support/auth/api/exception/LocalResponseStatusExceptionResolver.java
+++ src/main/java/com/lachesis/support/auth/api/exception/LocalResponseStatusExceptionResolver.java
@@ -13,19 +13,22 @@ public class LocalResponseStatusExceptionResolver extends AbstractHandlerExcepti
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception ex) {
-
+		response.setContentType("application/json; charset=utf-8");
 		try {
 			if (ex instanceof AuthenticationException) {
 				AuthenticationException authEx = (AuthenticationException) ex;
-				response.setContentType("application/json; charset=utf-8");
+				
 				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 				PrintWriter writer = response.getWriter();
 				writer.write(convertToJson(authEx));
 				writer.close();
-				// response.sendError(HttpServletResponse.SC_UNAUTHORIZED, );
+			}else{
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				PrintWriter writer = response.getWriter();
+				writer.write(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+				writer.close();
 			}
 		} catch (Exception handlerEx) {
-
 		}
 
 		return new ModelAndView();
